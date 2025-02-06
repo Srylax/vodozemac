bbfd084e1c9d06a080fde7768552a9328e38d214
Srylax <71783705+Srylax@users.noreply.github.com>
2025-02-06 16:45:29 +0100

    feat(types.ed25519): Add `Hash` to Ed25519PublicKey

--- src/types/ed25519.rs
+++ src/types/ed25519.rs
@@ -332,7 +332,7 @@ impl SecretKeys {
 }
 
 /// An Ed25519 public key, used to verify digital signatures.
-#[derive(Serialize, Deserialize, Clone, Copy, PartialEq, Eq)]
+#[derive(Serialize, Deserialize, Clone, Copy, PartialEq, Eq, Hash)]
 #[serde(transparent)]
 pub struct Ed25519PublicKey(VerifyingKey);
 
